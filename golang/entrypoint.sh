619334f7df4fc5bcf6bc018d0f0e3a0bb831ef1b
Will Hattingh <w.hattingh@nitecon.com>
2022-04-22 11:13:09 -0400

    fix: update to use semantic-version semantics

--- golang/entrypoint.sh
+++ golang/entrypoint.sh
@@ -2,20 +2,24 @@
 
 set -e
 
-if [[ -z "$APP_NAME" ]]; then
-  echo "Set the APP_VERSION env variable."
+if [[ ! -f ".VERSION" ]]; then
+  echo "Make sure semantic-version already ran to create .VERSION file"
   exit 1
 fi
-if [[ -z "$GITVERSION_SEMVER" ]]; then
+
+APP_VERSION=$(cat .VERSION)
+
+
+if [[ -z "$APP_VERSION" ]]; then
   echo "Make sure to run uses: gittools/actions/gitversion/execute@v0.9.7 before this"
   exit 1
 fi
 
-echo "Setting up for application version: ${GITVERSION_SEMVER}"
+echo "Setting up for application version: ${APP_VERSION}"
 BUILD_PATH=${GITHUB_WORKSPACE}/.build
 mkdir -p ${BUILD_PATH}
 
-GO111MODULE=on GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.version=${GITVERSION_SEMVER}" -o ${GITHUB_WORKSPACE}/.build/bin/${APP_NAME} cmd/main.go
+GO111MODULE=on GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.version=${APP_VERSION}" -o ${GITHUB_WORKSPACE}/.build/bin/${APP_NAME} cmd/main.go
 cd .build/ && zip -r ${APP_NAME}.zip ./*
 
 echo "Build is complete"
